caab23603b66974999f824c1409d025c19457867
Nishal Najeeb <nishal.najeeb@students.mq.edu>
2020-06-22 09:25:47 +1000

    intial layout added

--- timesync/src/App.js
+++ timesync/src/App.js
@@ -4,21 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React dsadasda
-        </a>
-      </header>
+    <div id = "add_members">
+      <h1>Add Members</h1>
+      <p>Name</p>
+      <input type="text" id="name"></input>
+      <p>City</p>
+		  <input type="text" id="city"></input>
+      <br></br>
+      <p></p>
+      <button type = "button" id = "add">ADD</button>
+	  	<button type = "button" id = "delete">DELETE</button>
+		  <button type = "button" id = "finish">FINISH</button>
     </div>
   );
 }
