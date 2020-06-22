b0d741cbb1f33f2bdf51ec0e33c8b428fbcdc625
Nishal Najeeb <nishal.najeeb@students.mq.edu>
2020-06-22 10:26:17 +1000

    Initial layout

--- timesync/src/App.js
+++ timesync/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div id = "add_members">
@@ -12,7 +14,7 @@ function App() {
 		  <input type="text" id="city"></input>
       <br></br>
       <p></p>
-      <button type = "button" id = "add">ADD</button>
+      <button type = "button" id = "add" >ADD</button>
 	  	<button type = "button" id = "delete">DELETE</button>
 		  <button type = "button" id = "finish">FINISH</button>
     </div>
