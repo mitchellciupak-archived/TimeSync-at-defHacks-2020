d4475d85dab85b1a023f124c0dc14c60575d02f8
terry <tzfeng@ucsd.edu>
2020-06-21 18:09:06 -0700

    Header

--- timesync/src/App.js
+++ timesync/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-
+import React from "react";
+import "./App.css";
+import Header from "./component/header";
 
 function App() {
-  return (
+    return (
+        <>
+            <Header dark={true} className="Header">
+                TimeSync
+            </Header>
 
-    <div id = "add_members">
-      <h1>Add Members</h1>
-      <p>Name</p>
-      <input type="text" id="name"></input>
-      <p>City</p>
-		  <input type="text" id="city"></input>
-      <br></br>
-      <p></p>
-      <button type = "button" id = "add" >ADD</button>
-	  	<button type = "button" id = "delete">DELETE</button>
-		  <button type = "button" id = "finish">FINISH</button>
-  );
+            <div id="add_members">
+                <h2>Add Members</h2>
+                <p>Name</p>
+                <input type="text" id="name"></input>
+                <p>City</p>
+                <input type="text" id="city"></input>
+                <br></br>
+                <p></p>
+                <button type="button" id="add">
+                    ADD
+                </button>
+                <button type="button" id="delete">
+                    DELETE
+                </button>
+                <button type="button" id="finish">
+                    FINISH
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default App;
